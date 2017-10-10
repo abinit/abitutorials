6e926d07c3fc2a5899194889dec68ad9bca8539f
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-10-10 21:31:04 +0200

    Add files for base1

--- abitutorials/base1/mkflow.py
+++ abitutorials/base1/mkflow.py
@@ -10,20 +10,33 @@ import abipy.data as abidata
 
 
 def gs_input(x=0.7, ecut=10, acell=(10, 10, 10)):
-    """H2 molecule in a big box"""
+    """
+    H2 molecule in a big box
+
+    Args:
+        x:
+        ecut:
+        acell
+
+    Returns:
+        AbinitInput object.
+    """
+    # Build structure from dictionary with input variables
     structure = abilab.Structure.from_abivars(
-        ntypat=1,
-        znucl=1,
-        natom=2,
-        typat=(1, 1),
-        xcart=[-x, 0.0, 0.0,
-               +x, 0.0, 0.0],
-        acell=acell,
-        rprim=[1, 0, 0, 0, 1, 0, 0, 0, 1]
+        ntypat=1,                           # There is only one type of atom
+        znucl=1,                            # Atomic numbers of the type(s) of atom
+        natom=2,                            # There are two atoms
+        typat=(1, 1),                       # They both are of type 1, that is, Hydrogen
+        xcart=[-x, 0.0, 0.0,                # Cartesian coordinates of atom 1, in Bohr
+               +x, 0.0, 0.0],               # second atom.
+        acell=acell,                        # Lengths of the primitive vectors (in Bohr)
+        rprim=[1, 0, 0, 0, 1, 0, 0, 0, 1]   # Orthogonal primitive vectors (default)
     )
 
+    # Build AbinitInput from structure and pseudo(s) taken from AbiPy package.
     inp = abilab.AbinitInput(structure=structure, pseudos=abidata.pseudos("01h.pspgth"))
 
+    # Set value of other variables.
     inp.set_vars(
         ecut=ecut,
         nband=1,
@@ -33,20 +46,21 @@ def gs_input(x=0.7, ecut=10, acell=(10, 10, 10)):
         iomode=3
     )
 
+    # Define k-point sampling.
     inp.set_kmesh(ngkpt=(1, 1, 1), shiftk=(0, 0, 0))
+
     return inp
 
 
 def build_flow(options):
     """
-    H2 molecule in a big box:
-    Generate a flow to compute the total energy and forces as a function of the interatomic distance
+    Generate a flow to compute the total energy and forces for the H2 molecule in a big box
+    as a function of the interatomic distance.
     """
-    workdir = options.workdir
-    if not options.workdir:
-        workdir = os.path.basename(__file__).replace(".py", "").replace("run_", "flow_")
+    workdir = "flow_h2" if options.workdir is None else options.workdir
 
     inputs = [gs_input(x=x) for x in np.linspace(0.5, 1.025, 21)]
+
     return flowtk.Flow.from_inputs("flow_h", inputs, remove=options.remove, pickle_protocol=0)
 
 
