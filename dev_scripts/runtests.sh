946a3b429acb0a05e168cd6189fc1b13ef1079d9
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-12-02 17:51:39 +0100

    Use pytest

--- dev_scripts/runtests.sh
+++ dev_scripts/runtests.sh
@@ -5,9 +5,10 @@ echo "PMG_MAPI_KEY: 8pkvwRLQSCVbW2Fe" > ${HOME}/.pmgrc.yaml
 
 abicheck.py --with-flow
 
-nosetests -v --with-coverage --cover-package=abipy --logging-level=INFO
+#nosetests -v --with-coverage --cover-package=abipy --logging-level=INFO
 #nosetests abipy -v --with-coverage --cover-package=abipy --logging-level=INFO
 
+pytest -v 
 #pytest --cov-config .coveragerc --cov=abipy -v  abipy # --doctest-modules 
 # This is to run the integration tests (append results)
 # pytest --cov-config .coveragerc --cov=abipy --cov-append -v abipy/integration_tests
